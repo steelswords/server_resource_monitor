21d529006862fd4ccf09fe0f1c7ab5298af5e2db
Tristan Andrus <tristan@tristanandrus.com>
2023-12-30 16:43:03 -0700

    Attempt to use libsensors
    Signed-off-by: Tristan Andrus <tristan@tristanandrus.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,63 @@
 use sys_info;
 use std::time::Duration;
+use sensors_sys::*;
 
-fn get_cpu_temperature() -> Result<f64, std::io::Error>
+/*
+ * void SensorData::FetchTemp()
 {
+    sensors_chip_name const * cn;
+    int c = 0;
+    while ((cn = sensors_get_detected_chips(0, &c)) != 0) {
+        std::cout << "Chip: " << cn->prefix << "/" << cn->path << std::endl;
+
+        sensors_feature const *feat;
+        int f = 0;
+
+        while ((feat = sensors_get_features(cn, &f)) != 0) {
+            std::cout << f << ": " << feat->name << std::endl;
+
+            sensors_subfeature const *subf;
+            int s = 0;
+
+            while ((subf = sensors_get_all_subfeatures(cn, feat, &s)) != 0) {
+                std::cout << f << ":" << s << ":" << subf->name
+                          << "/" << subf->number << " = ";
+                double val;
+                if (subf->flags & SENSORS_MODE_R) {
+                    int rc = sensors_get_value(cn, subf->number, &val);
+                    if (rc < 0) {
+                        std::cout << "err: " << rc;
+                    } else {
+                        std::cout << val;
+                    }
+                }
+                std::cout << std::endl;
+            }
+        }
+    }
+}
+*/
+fn get_cpu_temperature() -> Result<f64, String>
+{
+    /*
+    unsafe {
+        if 0 != sensors_init(std::ptr::null_mut())
+        {
+            return Err("Could not initialize sensors".to_string());
+        }
+        let mut chip_name: sensors_sys::sensors_chip_name;
+        let mut chip_number = 0;
+        // Loop over all chips libsensors can find
+        while sensors_get_detected_chips(std::ptr::null(), &mut chip_number) != std::ptr::null() {
+            println!("Chip: {}/{}", chip_name.prefix as &str, chip_name.path);
+            chip_number += 1;
+        }
+                     
+
+
+
+    }
+    */
     Ok(0.0)
 }
 
